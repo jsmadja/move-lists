37e9018724e0eaa70e8588f6c5724df9ac01622d
Julien Smadja <julien.smadja@gmail.com>
2015-01-24 18:29:15 +0100

    Fatal Fury Special

---
+++ js/garou-motw.js
@@ -0,0 +1,44 @@
+game = {
+    title: 'Garô: Mark Of The Wolves',
+    cover: 'http://www.fightersgeneration.com/games/garou-f.jpg',
+    characters: [
+        {
+            firstname: 'Terry',
+            lastname: 'Bogard',
+            face: 'http://wiki.basgrospoing.fr/images/4/43/Garou-terry.png',
+            moves: [
+                {name: 'Power Wave', commands: ['qcf', '+', 'A', 'ou', 'C']},
+                {name: 'Burn Knuckle', commands: ['qcb', '+', 'A', 'ou', 'C']},
+                {name: 'Power Dunk', commands: ['dp', '+', 'B', 'ou', 'D']},
+                {name: 'Crack Shoot', commands: ['qcb', 'ub', '+', 'B', 'ou', 'D']},
+                {name: 'Power Charge', commands: ['f', 'f', '+', 'A', 'ou', 'C', 'x3']}
+            ]
+        },
+        {
+            firstname: 'Terry',
+            lastname: 'Bogard',
+            face: 'http://wiki.basgrospoing.fr/images/4/43/Garou-terry.png',
+            moves: [
+                {name: 'Power Geyser S.Power', commands: ['qcf', 'qcf', '+', 'A']},
+                {name: 'Buster Wolf S.Power', commands: ['qcf', 'qcf', '+', 'B']},
+                {name: 'Power Geyser P.Power', commands: ['qcf', 'qcf', '+', 'C']},
+                {name: 'Buster Wolf P.Power', commands: ['qcf', 'qcf', '+', 'D']}
+            ]
+        }
+    ]
+}
+
+/*
+ {
+ firstname: '',
+ lastname: '',
+ face: '',
+ moves: [
+ {name: '', commands: []},
+ {name: '', commands: []},
+ {name: '', commands: []},
+ {name: '', commands: []},
+ {name: '', commands: []}
+ ]
+ }
+ */
